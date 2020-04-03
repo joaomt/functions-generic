baa20e0b9df956f2893e6ccc844e1c44c9ec2800
João Vitor <joao_vitormt@hotmail.com>
2020-04-03 16:00:02 -0300

    first commit

---
+++ functionsgenericlibrary/src/main/java/com/mebill/functionsgenericlibrary/EF_Animation.kt
@@ -0,0 +1,39 @@
+package com.mebill.functionsgenericlibrary
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import recod.company.soft.mebill.R
+
+fun View.loadAnim(anim: Int) {
+    startAnimation(AnimationUtils.loadAnimation(this.context, anim))
+}
+
+fun View.startTranslationY(value: Float) {
+    this.animate().translationY(value).start()
+}
+
+fun View.startTranslationX(value: Float, duration: Long) {
+    this.animate().translationX(value).setDuration(duration).start()
+}
+
+fun Context.showToastLengthShort(msg: String) {
+    Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+}
+
+fun Context.showToastLengthLong(msg: String) {
+    Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+}
+@SuppressLint("ResourceAsColor")
+fun View.showSnackBar(message:String, colorRes : Int){
+    Snackbar.make(this,message,Snackbar.LENGTH_LONG).also {snackbar ->
+        snackbar.view.setBackgroundColor(colorRes)
+        snackbar.setActionTextColor(context.resources.getColor(R.color.colorWhite))
+        snackbar.setAction("OK"){
+            snackbar.dismiss()
+        }
+    }.show()
+}
